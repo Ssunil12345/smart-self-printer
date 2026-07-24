40b0b7233ab2ed8ab7a66670beb84b348d431cd9
Sunilkumar S <113884961+Ssunil12345@users.noreply.github.com>
2026-07-24 12:57:01 +0530

    Update baseUrl to an empty string

--- lib/core/network/api_endpoints.dart
+++ lib/core/network/api_endpoints.dart
@@ -1,7 +1,7 @@
 class ApiEndpoints {
   ApiEndpoints._();
 
-  static const String baseUrl = 'https://aislyntech.com/Api/';
+  static const String baseUrl = '';
   static const String upload = '${baseUrl}30-update.php';
   static const String paymentSuccess = '${baseUrl}31-update.php';
 
